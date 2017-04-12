56f1c0bd38e3baebbb77dae6bb961d33a9431bb1
Abhishek Gupta <abhishekgupta@Abhisheks-MacBook-Pro.local>
2017-04-12 00:47:41 -0400

    Start

---
+++ BallsEnemy/AppDelegate.h
@@ -0,0 +1,17 @@
+//testingg
+//  AppDelegate.h
+//  BallsEnemy
+//
+//  Created by Abhishek Gupta on 4/11/17.
+//  Copyright © 2017 Abhishek Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BallsEnemy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BallsEnemy
+//
+//  Created by Abhishek Gupta on 4/11/17.
+//  Copyright © 2017 Abhishek Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
