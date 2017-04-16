a1e388bd07cf59dc30ed7741c0d12c44acafd48a
Dipankar Ghosh <dipankarghosh@Dipankars-MacBook-Air.local>
2017-04-15 20:46:42 -0400

    StartofGame

--- BallsEnemy/AppDelegate.h
+++ BallsEnemy/AppDelegate.h
@@ -9,12 +9,23 @@
 //  Created by Abhishek Gupta on 4/11/17.
 //  Copyright © 2017 Abhishek Gupta. All rights reserved.
 //
-
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class ViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
 
 
 @end
