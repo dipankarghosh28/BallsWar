b0974c5fb8424e600f39da858041f50d19f4b07c
Dipankar Ghosh <dipankarghosh@Dipankars-MacBook-Air.local>
2017-04-16 17:37:48 -0400

    Accelerator for ball

--- BallsEnemy/EnemyBall.m
+++ BallsEnemy/EnemyBall.m
@@ -10,13 +10,6 @@
 
 @implementation EnemyBall
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
 -(UIImageView *)addEnemyBallFromClass{
     
     UIViewController *myView = [[UIViewController alloc]init];
--- BallsEnemy/ViewController.m
+++ BallsEnemy/ViewController.m
@@ -25,9 +25,7 @@ AVAudioPlayer *audioPlayer;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    highScoreNumber = [[NSUserDefaults standardUserDefaults]integerForKey:@"highScoreSaved"];
+       highScoreNumber = [[NSUserDefaults standardUserDefaults]integerForKey:@"highScoreSaved"];
     
 }
 
@@ -44,7 +42,6 @@ AVAudioPlayer *audioPlayer;
     addMoreBall = [NSTimer scheduledTimerWithTimeInterval:3 target:self selector:@selector(addMoreBall) userInfo:nil repeats:YES];
     
     
-    //Add player ball
     self.player = [[UIImageView alloc]initWithFrame:CGRectMake(self.view.center.x, self.view.center.y, 40, 40)];
     UIImage *playerImage = [UIImage imageNamed:@"playerball"];
     self.player.image = playerImage;
@@ -109,10 +106,8 @@ AVAudioPlayer *audioPlayer;
             
             CGPoint posistion = [speedArray[index] CGPointValue];
             
-            view.center = CGPointMake(view.center.x + posistion.x, view.center.y + posistion.y);    //movement of the enemyball
-            
-            if (view.center.x > self.view.frame.size.width || view.center.x <0) {  //when it hits the boundary condtion. Enemyball will move negaitve value.
-                
+            view.center = CGPointMake(view.center.x + posistion.x, view.center.y + posistion.y);
+            if (view.center.x > self.view.frame.size.width || view.center.x <0) {
                 CGPoint tempSpeed = CGPointMake(-posistion.x, posistion.y);
                 [speedArray replaceObjectAtIndex:index withObject:[NSValue valueWithCGPoint:tempSpeed]];
             }
@@ -130,17 +125,13 @@ AVAudioPlayer *audioPlayer;
                 
                 if ([anotherView isKindOfClass:[UIImageView class]] && (anotherView != self.player) && (anotherView != view)) {
                     
-                    if (CGRectIntersectsRect(anotherView.frame, view.frame)) { //checking the collision between enemyballs && condition to make sure they are not the same view
-                        /********************Check how they intersect here**************************/
-                        
-                        //deflection of anotherView
+                    if (CGRectIntersectsRect(anotherView.frame, view.frame)) {
                         CGPoint anotherViewPosition = [speedArray[indexAnotherView] CGPointValue];
                         CGPoint tempAnotherViewSpeed = CGPointMake(-anotherViewPosition.x, -anotherViewPosition.y);
                         [speedArray replaceObjectAtIndex:indexAnotherView withObject:[NSValue valueWithCGPoint:tempAnotherViewSpeed]];
                         
-                        //deflecton of view
-                        CGPoint posistion = [speedArray[index] CGPointValue];                 //something wrong with this index
-                        CGPoint tempViewSpeed = CGPointMake(-posistion.x, -posistion.y);
+                    CGPoint posistion = [speedArray[index] CGPointValue];
+                    CGPoint tempViewSpeed = CGPointMake(-posistion.x, -posistion.y);
                         [speedArray replaceObjectAtIndex:index withObject:[NSValue valueWithCGPoint:tempViewSpeed]];
                     }
                     indexAnotherView++;
@@ -156,13 +147,11 @@ AVAudioPlayer *audioPlayer;
     
     NSArray *subview = [self.view subviews];
     
-    //check collision for all the enemy balls
-    for (UIView *viewInSub in subview) {
+       for (UIView *viewInSub in subview) {
         
         if ([viewInSub isKindOfClass:[UIImageView class]] && (viewInSub != self.player)) {
             
-            if (CGRectIntersectsRect(self.player.frame, viewInSub.frame)) {                  //Perform these once player intersects with any enemy
-                
+            if (CGRectIntersectsRect(self.player.frame, viewInSub.frame)) {
                 [randomMain invalidate];
                 [startButton setHidden:NO];
                 [highScoreLabel setHidden:NO];
@@ -171,8 +160,7 @@ AVAudioPlayer *audioPlayer;
                 [addMoreBall invalidate];
                 [self removeSubView];
                 
-                //Display ScoreNumber first
-                [highScoreLabel setText:[NSString stringWithFormat:@"Score"]];
+                               [highScoreLabel setText:[NSString stringWithFormat:@"Score"]];
                 [highScore setText:[NSString stringWithFormat:@"%d",scoreNumber]];
                 
                 UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"You Lost" message:@"You are hit. Try Again!" delegate:self cancelButtonTitle:@"Dismiss" otherButtonTitles:nil, nil];
@@ -191,8 +179,7 @@ AVAudioPlayer *audioPlayer;
     if (buttonIndex == 0) {
         NSLog(@"This is performed");
         [highScoreLabel setText:[NSString stringWithFormat:@"High Score"]];
-        
-        //display the highscore values
+       
         if (scoreNumber > highScoreNumber) {
             [[NSUserDefaults standardUserDefaults]setInteger:scoreNumber forKey:@"highScoreSaved"];
         }
@@ -207,12 +194,72 @@ AVAudioPlayer *audioPlayer;
 -(void)removeSubView {
     NSArray *subViews = [self.view subviews];
     for (UIView *view in subViews) {
-        if ([view isKindOfClass:[UIImageView class]]) { //remove enemy and player balls. Once start button is pressed, player ball will be added in.
+        if ([view isKindOfClass:[UIImageView class]]) {
             [view removeFromSuperview];
         }
     }
 }
 
-///////////////////////
+-(void)startAcceleratorForPlayer {
+    
+    self.motionManager = [[CMMotionManager alloc]init];
+    
+    self.motionManager.accelerometerUpdateInterval = 0.01;
+    
+    if ([self.motionManager isAccelerometerAvailable]) {
+        
+        NSOperationQueue *queue = [[NSOperationQueue alloc]init];
+        [self.motionManager startAccelerometerUpdatesToQueue:queue withHandler:^(CMAccelerometerData * accelerometerData, NSError * _Nullable error) {
+            
+            NSLog(@"X = %0.4f, Y = %.04f, Z = %0.4f",
+                  accelerometerData.acceleration.x,
+                  accelerometerData.acceleration.y,
+                  accelerometerData.acceleration.z);
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                float valueX = accelerometerData.acceleration.x * 30.0;
+                float valueY = accelerometerData.acceleration.y * 30.0;
+                
+                int intPlayerNewPosX = (int)(self.player.center.x + valueX);
+                int intPlayerNewPosY = (int)(self.player.center.y - valueY);
+                
+               
+                if (intPlayerNewPosX > (self.view.frame.size.width - MovingObjectRadius)) {
+                    intPlayerNewPosX = (self.view.frame.size.width - MovingObjectRadius);
+                }
+                
+                if (intPlayerNewPosX < (0 + MovingObjectRadius)) {
+                    intPlayerNewPosX = (0 + MovingObjectRadius);
+                }
+                
+                if (intPlayerNewPosY > (self.view.frame.size.height - MovingObjectRadius)) {
+                    intPlayerNewPosY = (self.view.frame.size.height - MovingObjectRadius);
+                }
+                
+                if (intPlayerNewPosY < (0 + MovingObjectRadius)) {
+                    intPlayerNewPosY = (0+ MovingObjectRadius);
+                }
+                
+                CGPoint playerNewPoint = CGPointMake(intPlayerNewPosX, intPlayerNewPosY);
+                self.player.center = playerNewPoint;
+                
+            });
+        }];
+    } else{
+        NSLog(@"Not Active.");
+    }
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event{
+    UITouch *myTouch = [[event allTouches]anyObject];
+    self.player.center = [myTouch locationInView:self.view];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
 
 @end
